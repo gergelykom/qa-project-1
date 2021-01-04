0f2ac4bb9971f2ebf1cf1e6d2617768a6faac297
gergelykom <gergelykomuves@gmail.com>
2021-01-04 20:06:40 +0000

    changes

--- qa-project-0.6/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DATABASE_URI='mysql+pymysql://root:groot@35.234.132.64/flask_db'
-cd /opt/qa-project
-sudo python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 app.py
--- qa-project-0.7/application/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
-from wtforms import StringField, SubmitField
-
-class TaskForm(FlaskForm):
-    description = StringField('Proposed Name', validators = [DataRequired()])
-    
-    submit = SubmitField('Add Species')
