1a4d4b9cead29cf015b80a7dfa13ff3f12a94739
gergely_komuves <70955431+gergelykom@users.noreply.github.com>
2021-01-04 20:26:25 +0000

    Update test_unit.py

--- qa-project-0.8/tests/test_unit.py
+++ qa-project-0.8/tests/test_unit.py
@@ -2,8 +2,8 @@ import unittest
 import pytest
 from flask import url_for
 from flask_testing import TestCase
-//from application import app, db
-//from application.models import Species
+#from application import app, db
+#from application.models import Species
 
 
 class TestBase(TestCase):
